8944b426b62f6b3c2efa8a725fd701ba0c2d71f6
JangHyeongyu <47076471+JangHyeongyu1@users.noreply.github.com>
2025-04-04 16:25:17 +0900

    최소힙(eddited)

--- 1927_최소힙.cpp
+++ 1927_최소힙.cpp
@@ -1,41 +1,38 @@
 #include <iostream>
+#include <queue>
 #include <vector>
-#include <climits>
 using namespace std;
 
 int main() {
-	int N;	
-	cin >> N;
-	vector<int> input(N, -1);
-	int x;
-	int min;
-	vector<int> result(N);
-	int result_index = 0;
-	int min_index;
+    int N;
+    cin >> N;
 
-	for (int i = 0; i < N; i++) {
-		cin >> x;
-		min = INT_MAX;
+    vector<int> result;
 
-		if (x == 0) {
-			for (int j = 0; j < N; j++) {
-				if (input[j] < min && input[j] != -1) {
-					min = input[j];
-					min_index = j;
-				}
-			}
-			result[result_index++] = min;
-			input[min_index] = -1;	
-		}
-		else {
-			input[i] = x;
-		}
-		
-	}
+    // 최소 힙을 위한 priority_queue, 기본적으로 최대 힙이므로 우선순위 반대로 설정.
+    priority_queue<int, vector<int>, greater<int>> minHeap;
 
-	for (int i = 0; i < result_index; i++) {
-		cout << result[i] << endl;
-	}
-	
-	return 0;
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+
+        if (x == 0) {
+            if (minHeap.empty()) {
+                result.push_back(0);
+            }
+            else {
+                result.push_back(minHeap.top());
+                minHeap.pop();
+            }
+        }
+        else {
+            minHeap.push(x);
+        }
+    }
+
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << endl;
+    }
+
+    return 0;
 }
